ef3a68ae102f05e00de3a87b5d4e49715c30f06b
Oulahyane Abdellah <aitbaha48@gmail.com>
2024-03-10 17:03:41 +0300

    --

--- vendor/core/DB/Contracts/IQueryBuilder.php
+++ vendor/core/DB/Contracts/IQueryBuilder.php
@@ -6,17 +6,20 @@ namespace Core\DB\Contracts;
 
 interface IQueryBuilder{
     public static function query() : IQueryBuilder; 
-    public static function where(array|callable $condition) : IQueryBuilder; 
-    public static function orWhere(array|callable $condition) : IQueryBuilder; 
-    public static function find(mixed $primaryKey) : IQueryBuilder; 
-    public static function all() : IQueryBuilder; 
-    public static function get() : array; 
-    public static function first() : object|array; 
-    public static function join() : IQueryBuilder; 
-    public static function orderBy() : IQueryBuilder; 
-    public static function groupBy() : IQueryBuilder; 
-    public static function when() : IQueryBuilder; 
-    public static function whereHas() : IQueryBuilder; 
-    public static function select(array $columns) : IQueryBuilder; 
-    public static function pluck(string $column) : object|array; 
+    public function where(array|callable $condition) : IQueryBuilder; 
+    public function orWhere(array|callable $condition) : IQueryBuilder; 
+    public function find(mixed $primaryKey) : IQueryBuilder; 
+    public function all() : IQueryBuilder; 
+    public function get() : array; 
+    public function first() : object|array; 
+    public function join() : IQueryBuilder; 
+    public function orderBy() : IQueryBuilder; 
+    public function groupBy() : IQueryBuilder; 
+    public function when() : IQueryBuilder; 
+    public function whereHas() : IQueryBuilder; 
+    public function select(array $columns) : IQueryBuilder; 
+    public function pluck(string $column) : object|array; 
+    public function selectRaw(string $sql, array $params): IQueryBuilder;
+    public function update(array $columns) : mixed;
+    public static function create(array $columns) : mixed;
 }
